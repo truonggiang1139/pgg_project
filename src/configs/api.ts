fdf14e07b49a122e554f0eab1dbf7e0ad7e6fb9d
Hoang Giang <truonggiang1139@gmail.com>
2023-06-05 18:01:19 +0700

    5/6

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -4,6 +4,7 @@ export const API_PATHS = {
   signIn: `${API_HOST}/login`,
   forgotPassword: `${API_HOST}/forgot-password`,
   logOut: `${API_HOST}/logout`,
+
   detail: `${API_HOST}/user/detail`,
   employee: `${API_HOST}/employee`,
   marriage: `${API_HOST}/marriage`,
